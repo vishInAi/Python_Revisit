529280b76a39f5b4df010754b19410fb48100a29
vishInAi <vishweshpanchal00@gmail.com>
2024-08-02 19:39:57 +0530

    just uploaded a post right now watching lecture 1

---
+++ core_concepts/L1_Variables&dataTypes.py
@@ -0,0 +1,30 @@
+# welcome_message
+print("Welcome to this Quick Revisit into Python")
+
+# var assignment of diff data types
+name="Vishwesh"
+age=20
+price=60000000000.00
+# Apollo Hospitals states that the notional value of a single human body could be as high as $6,000,000,000 😂
+
+print("\n Variables declared above and their values : ")
+# dont mind me i use string literals all the time cause you dont have to put in more "" to put in string in a bit bit manner saves time
+print(f" Name : {name}")
+print(f" Age : {age}")
+print(f" Price : {price}")
+
+# Python Characterset
+letters ="A to Z & a to z"
+digits ="0 to 9"
+special_symbols ="+,-,/,*,etc."
+whitespaces ="Blank Space, tab, carriage return, newline, formfeed"
+# Ascii and unicode are compatible too
+
+print(f"\n Python Charactersets : ")
+print(f" Letters : {letters}")
+print(f" Digits : {digits}")
+print(f" Special Symbols : {special_symbols}")
+print(f" Whitespaces : {whitespaces}")
+print(f" Ascii and unicode are also compatible : 😂")
+
+
