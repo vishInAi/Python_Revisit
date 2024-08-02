6d70aafedf1daa27d2c51055e36065ad29a0892c
vishInAi <vishweshpanchal00@gmail.com>
2024-08-02 16:10:22 +0530

    if else

---
+++ if else.py
@@ -0,0 +1,21 @@
+#elif testing
+num=int(input("enter the value of num: "))
+if (num<0):
+    print("number is negative")
+elif(num==0):
+    print("no is zero")
+elif((num<20)>0):
+    print("no is between 0-20")
+else:
+    print("number is bigger than 20")
+
+        
+        
+        
+    
+
+
+
+
+    
+    
