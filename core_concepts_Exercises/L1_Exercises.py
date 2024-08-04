1c2cf6e2217c2aaa71ff422362ef20dd39be8980
vishInAi <vishweshpanchal00@gmail.com>
2024-08-04 16:59:07 +0530

    Lecture 1 Exercises

---
+++ core_concepts_Exercises/L1_Exercises.py
@@ -0,0 +1,38 @@
+# Lecture 1 Exercises
+
+# WAP to Input two numbers and get their sum printed
+print("\n WAP to Input two numbers and get their sum printed")
+num1 = float(input("\n Enter First Number : "))
+num2 = float(input("\n Enter Second Number : "))
+sum = num1 + num2
+print(f"The sum of both the numbers inputted by user : {sum}")
+
+# WAP to input side of a square and print its area
+print("\n WAP to input side of a square and get its area")
+side = float(input("Enter side of a square : "))
+area = side * side
+print(f"\n The area of the square with side {side} is : {area}")
+# havent added units cause you input one unit you get area in exactly that unit
+# eg you input in cm you get area in cm^2
+
+# WAP to input 2 floating point numbers and print their average
+print("\n Program to input 2 floating point numbers and print their average : ")
+numa1 = float(input("enterFirstNumber : "))
+numa2 = float(input("enterSecondNumber : "))
+avg = (numa1 + numa2)/2
+print(f"\n The average of the two above inputted numbers {numa1} and {numa2} is : {avg}")
+
+# WAP to input two numbers and see if first is greater than or equal to second
+print("\n WAP to input two numbers and see if first is greater than or equal to second : ")
+a = float(input("Enter first integer (a): "))
+b = float(input("Enter first integer (b): "))
+print(f"Moment of Truth : {'True' if a>=b else 'false'}")
+
+print("\n Lecture 1 exercises over ....")
+
+
+
+
+
+
+
