894f50d1f21843801e7503cf4ccf16dc2719cac4
vishInAi <vishweshpanchal00@gmail.com>
2024-08-09 22:42:43 +0530

    11 pm im done for now

---
+++ 1_core_concepts/L5_Loops.py
@@ -0,0 +1,10 @@
+# Loops in Python
+
+# While Loop
+count = 1
+
+while count <= 5 :
+    print("Hello world")
+    count+=1
+
+print(count)    
---
+++ 2_core_concepts_Exercises/L5_Exercises.py
@@ -0,0 +1,23 @@
+# While Loop Exercises
+
+# Program 1 : print numbers 1 to 100
+i = 1
+while i <= 100:
+    print(i)
+    i+=1
+
+# Program 2 : print numbers 100 to 1
+while i>=1 : 
+    print(i)
+    i-=1
+
+# Program 3 : Print Multiplication Table of a number n
+
+num = int(input("input a number you want table of : "))
+j = 1
+while j<=10:
+    print(num*j)
+    j+=1
+
+
+
