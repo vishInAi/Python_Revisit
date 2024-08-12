90a0008e4fbd55d93b695e4db2dad03859391326
vishInAi <vishweshpanchal00@gmail.com>
2024-08-12 22:27:34 +0530

    good night

--- 1_core_concepts/L5_Loops.py
+++ 1_core_concepts/L5_Loops.py
@@ -7,4 +7,68 @@ while count <= 5 :
     print("Hello world")
     count+=1
 
-print(count)    
+print(count)
+
+# Block & Continue
+
+count = 1
+while count<=5 :
+    print("Hello World")
+    if count == 3 :
+        print("count reached 3")
+        break
+    count += 1
+
+count = 0
+nums = (1, 4, 9, 16, 25, 36, 49, 64, 81,100)
+num_search = int(input("Enter the Number you want to search : "))
+while count < len(nums) :
+    print ("Looking for it ...")
+    if nums[count] == num_search :
+        print ("\n Found the number 🍌")
+        print (f"\n The number you were looking for {nums[count]} is at index {count} ")
+        break
+    count += 1
+else :
+    print ("\n Number not found 🥕") 
+# use break wisely its not a switch statement its just for some exceptions if you wanna use it
+
+# lets use continue now
+
+count = 0
+while count <= 10 :
+    if count%3 == 0 :
+        count +=1
+        continue
+    print (f" numbers : {count}")
+    count +=1
+# its simply used to keep the loop going by skipping certain stuff you wanna skip
+
+# lets see for loop now
+count = 0
+veggies = ["potatoes","brinjal","cucumber","ladysfinger"]
+for char in veggies:
+    print (veggies[count])
+    count+=1
+str_veggie = "potatoes"
+for char in str_veggie:
+    print(char)
+    if char == "o":
+        print("found o")
+        break
+else:
+    print("End")
+
+# Range stuff in Loops
+
+for el in range(5): # range(stop)
+    print(el)
+for el in range(1,5): # range(start, stop)
+    print(el)
+for el in range(1,5,2): # range(start, stop, step)
+    print(el)
+# its late night ill update it whenever i get time or atleast put up a better example for the range 
+# range(start?,stop,step?)
+
+
+
--- 2_core_concepts_Exercises/L5_Exercises.py
+++ 2_core_concepts_Exercises/L5_Exercises.py
@@ -25,9 +25,41 @@ while j<=10:
 
 num_tup = (1, 4, 9, 16, 25, 36, 49, 64, 81,100)
 k = 0
-print(f" All the Numbers till x :")
+print(f"\nAll the Numbers till x :")
 while k<=9 :
     print(num_tup[k])
     k+=1
+
+# program 5 : for loop : Print the elements fo the following list using a loop
+# [1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+
+print("\nPrinting Numbers in the List : ")
+num_list = [1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+for num in num_list:
+    print(num)
+    num+=1
+
+# program 6 : for loop : searching for number x in the from the tuple 
+# (1, 4, 9, 16, 25, 36, 49, 64, 81,100)
+
+print("\nSearching number from within the Tuple ")
+num_find = int(input("\nInput the number you want to find : "))
+for i in range(len(num_tup)):
+    print("Working on it...")
+    if num_tup[i] == num_find:
+        print (f"See what we found : {num_tup[i]} at index {i}")
+        break
+else:
+    print("\nWe didnt find the Number 🍌")
+
+# program 7 : for loop : print numbers 1 to 100
+
+for j in range(1,101):
+    print(j)
+
+# program 8 : for loop : print numbers 100 to 1
+
+# enough for today
+
 
 
