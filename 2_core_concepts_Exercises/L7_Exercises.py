ea58c70aa074116a9c97c50d9e2f32eaea493089
vishInAi <vishweshpanchal00@gmail.com>
2024-10-08 23:17:06 +0530

    few exercises

--- 2_core_concepts_Exercises/L7_Exercises.py
+++ 2_core_concepts_Exercises/L7_Exercises.py
@@ -18,3 +18,41 @@ print(update)
 
 with open ("practice.txt", "w") as f:
     f.write(update)
+
+# search if the word learning exists in the file or not
+word = str(input("tell what to search : "))
+with open("temp.txt", "r") as find:
+    data = find.read()
+    if (data.find(word) != 1):
+        print("found")
+    else:
+        print("not found 🍌")
+
+# WAF to find in whick of the file does the word "learning" occur first print -1 if word not found.
+
+def check():
+    words = "learning"
+    data = True
+    line_no = 1
+    with open("practice.txt", "r") as f:
+        while data:
+            data = f.readline()
+            if (word in data):
+                print(line_no)
+            line_no += 1
+    return -1
+
+print(check())
+
+# from a file containing numbers seperated by commas, print the count of even numbers.
+count = 0
+with open("practice1.txt", "r") as even:
+    data = even.read()
+    print(data)
+
+    nums = data.split(",")
+    for val in nums:
+        if (int(val) % 2 == 0):
+            count += 1
+
+print(count)
